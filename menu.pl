b1929911e3449c4d64d6241f251a8ae48b094f98
Klaywert <klaywert.souza@ccc.ufcg.edu.br>
2019-11-28 19:15:34 -0300

    Adicionei alguns métodos de menu, sendo que alguns nao estão implementados ou incompletos

--- menu.pl
+++ menu.pl
@@ -1,6 +1,6 @@
 menu :- write('***************************** \n'),
         write('********** SUDOKU ***********'), nl,
-        write('*****************************'), nl
+        write('*****************************'), nl,
         opcoes.
 
 
@@ -14,3 +14,21 @@ escolheOpcao(Opcao) :- Opcao == 1, write('Metodo de gerar o sudoku');
                        Opcao == 2, write('Resolvedor de sudoku');
                        Opcao == 3, true;
                        Opcao == _, write('Opçao invalida, tente novamente.'), opcoes.
+                       
+escolheCoordenadas(SudokuGerado) :- mostrarSudoku(SudokuGerado),
+									write('Escolha a linha (0 para desistir): '),
+									read(Linha), nl,
+									write('Escolha a coluna (0 para desistir): '),
+									read(Coluna), nl,
+									write('Insira o numero: '),
+									read(Numero), nl,
+									verificaCoordenadas(SudokuGerado, Linha,Coluna,Numero).
+									
+verificaCoordenadas(SudokuGerado ,Linha, Coluna, Numero) :- Linha == 0, Coluna == 0, verificaDesistencia (SudokuGerado,-1,-1, Numero);
+											 
+
+verificaDesistencia(Matriz (-1) (-1) Numero, Retorno) =  Retorno is backTrackingResolution(Matriz (0,0) 1).
+verificaDesistencia(Matriz, Linha, Coluna, Numero, Retorno) = Retorno is adicionaElementoPosicao Matriz (0,0) (Linha,Coluna) Numero)
+                       
+
+
