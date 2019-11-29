fa57b38eb691c632694009d294264b203e513d78
caio-fm <caio.moreira@ccc.ufcg.edu.br>
2019-11-29 14:08:31 -0300

    aa

--- sudoku.pl
+++ sudoku.pl
@@ -1,20 +1,42 @@
 :-use_module(library(clpfd)).
 
 solve() :-
-board(Linhas),
-sudoku(Linhas),
-maplist(portray_clause, Linhas).
+    board(Linhas),
+    sudoku(Linhas),
+    maplist(portray_clause, Linhas).
 
 generate() :-
-random_line(L),
-generatedBoard(Linhas),
-nth1(1,Linhas,L),
-sudoku(Linhas),
-maplist(label, Linhas),
-maplist(portray_clause, Linhas).
+    random_line(L),
+    generatedBoard(Linhas),
+    nth1(1,Linhas,L),
+    sudoku(Linhas),
+    maplist(label, Linhas),
+    zeraCelulas(Linhas, 0, M),
+    maplist(portray_clause, M).
 
 random_line(L) :-
-random_permutation([1,2,3,4,5,6,7,8,9],L).
+    random_permutation([1,2,3,4,5,6,7,8,9],L).
+
+index(Matrix, Row, Col, Value):-
+  nth1(Row, Matrix, MatrixRow),
+  nth1(Col, MatrixRow, Value).
+
+replaceElementM(R, C, Mat, Val, Upd) :-
+    nth1(R, Mat, OldRow, RestRows),   
+    nth1(C, OldRow, _Val, NewRow),    
+    nth1(C, NewRowUpd,Val,NewRow),	
+    nth1(R, Upd, NewRowUpd, RestRows).
+
+zeraCelulas(MatrizJogadorCopiada,15,MatrizJogador):-
+	MatrizJogador = MatrizJogadorCopiada.
+	
+zeraCelulas(MatrizJogadorCopiada,NumCelulasZeradas,MatrizJogador):-
+	random_between(1, 9, Row),
+	random_between(1, 9, Col),
+	index(MatrizJogadorCopiada_,Row,Col,Valor),
+	(Valor == 0 -> zeraCelulas(MatrizJogadorCopiada_,NumCelulasZeradas,MatrizJogador);
+								replaceElementM(Row,Col,MatrizJogadorCopiada,_,MatrizJogadorCopiada_),NewNumCelulas is (NumCelulasZeradas + 1)
+								,zeraCelulas(MatrizJogadorCopiada_,NewNumCelulas,MatrizJogador)).
 
 board(
     [[_,6,_,_,_,5,_,_,_],
@@ -28,15 +50,15 @@ board(
      [8,_,_,_,4,_,6,1,_]]).
 
 generatedBoard(
-        [[_,_,_,_,_,_,_,_,_],
-         [_,_,_,_,_,_,_,_,_],
-         [_,_,_,_,_,_,_,_,_],
-         [_,_,_,_,_,_,_,_,_],
-         [_,_,_,_,_,_,_,_,_],
-         [_,_,_,_,_,_,_,_,_],
-         [_,_,_,_,_,_,_,_,_],
-         [_,_,_,_,_,_,_,_,_],
-         [_,_,_,_,_,_,_,_,_]]).
+    [[_,_,_,_,_,_,_,_,_],
+     [_,_,_,_,_,_,_,_,_],
+     [_,_,_,_,_,_,_,_,_],
+     [_,_,_,_,_,_,_,_,_],
+     [_,_,_,_,_,_,_,_,_],
+     [_,_,_,_,_,_,_,_,_],
+     [_,_,_,_,_,_,_,_,_],
+     [_,_,_,_,_,_,_,_,_],
+     [_,_,_,_,_,_,_,_,_]]).
 
 sudoku(Linhas) :-
     length(Linhas, 9),  /*São 9 linhas*/
